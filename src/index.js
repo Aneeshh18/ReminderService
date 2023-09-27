b7fb084edb6ed9bba09952dc1f272c0b22403e9f
Aneesh Sahu <88325646+Aneeshh18@users.noreply.github.com>
2023-09-27 19:55:37 +0530

    Setup of smtp using nodemailer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require("./services/email-service");
+
 const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
@@ -10,7 +12,13 @@ const setupAndStartServer = async () => {
 
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-        
+
+        sendBasicEmail(
+            'support@admin.com',
+            'aneesh9169@gmail.com',
+            'This is a test email ',
+            'Hey, how are you i hope you like the support service'
+        );
     });
 }
 
