8085cbb0f953c997d3477238c86b4121b2b8a684
Aneesh Sahu <88325646+Aneeshh18@users.noreply.github.com>
2023-09-27 21:02:29 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require("./services/email-service");
+// const { sendBasicEmail } = require("./services/email-service");
 
 const setupAndStartServer = async () => {
     const app = express();
@@ -13,12 +13,12 @@ const setupAndStartServer = async () => {
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
 
-        sendBasicEmail(
-            'support@admin.com',
-            'aneesh9169@gmail.com',
-            'This is a test email ',
-            'Hey, how are you i hope you like the support service'
-        );
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'notificaitonservice@gmail.com',
+        //     'This is a test email ',
+        //     'Hey, how are you i hope you like the support service'
+        // );
     });
 }
 
